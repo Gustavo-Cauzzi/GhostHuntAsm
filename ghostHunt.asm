257ba5ed85577dafdffee26e184951b6d4d76d86
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2022-11-13 21:11:59 -0300

    show pixel base proc

--- ghostHunt.asm
+++ ghostHunt.asm
@@ -16,8 +16,24 @@
            db '           / / / /_  ______  / /_ ',10,13
            db '          / /_/ / / / / __ \/ __/ ',10,13
            db '         / __  / /_/ / / / / /_   ',10,13
-           db '        /_/ /_/\__,_/_/ /_/\__/   $',10,13           
-                                    
+           db '        /_/ /_/\__,_/_/ /_/\__/   ',10,13           
+    title1_len EQU $-title1
+                                                                 
+    GHOST_MASK db '   xxxxxx   ',
+               db '  xxxxxxxx  ',
+               db ' xxxxxxxxxx ',
+               db 'xxx  xx  xxx',
+               db 'xxx  xx  xxx',
+               db 'xxxxxxxxxxxx',
+               db 'xxxxxxxxxxxx',
+               db 'xxxxxxxxxxxx',
+               db 'xxx xxxx xxx',
+               db ' x   xx   x '
+    GHOST_MASK_WIDTH_LEN db 12
+    GHOST_MASK_HEIGHT_LEN db 10    
+    
+    VIDEO_BUFFER_SEGMENT dw 40960
+                                                                 
     ; Tela inicial
     TITLE_SELECTED_BTN db 1 ; 1 ou 2     
     IS_GAME_TITLE_RENDERED db 0 ; 0 ou 1
@@ -62,28 +78,28 @@ TEXT_MODE proc
 TEXT_MODE endp 
         
 ; --------------------------------------------------------------  
-; Mostra o título na tela inicial         
+; Mostra o t?tulo na tela inicial         
 proc SHOW_TITLE
     push DX
     push CX
     push BX
-    push AX
-             
-    mov AH, 09H   
-    mov CX, 1000H
-    mov BL, 0AH
-    int 10
-             
-    mov DL, 0
-    mov DH, 4
-    call SET_CURSOR
-     
-    mov DX, offset title1          ; Salva endereco inicial do titulo em bx  
-    mov AH, 09h
-    int 21h
+    push AX     
+                      
+    mov AL, 0 ; https://i.pinimg.com/736x/9d/4a/3b/9d4a3b8bece01eb5aef7a78eb0d7be93.jpg
+    mov BH, 0 ; Número da página
+    mov BL, 2 ; Cor => 02 = Fundo preto e texto verde
+    
+    mov DH, 4              ; Linha
+    mov DL, 0              ; Coluna
+    mov CX, title1_len     ; Tamanho
+    mov BP, offset title1  ; Endereço
+    
+    ; https://en.wikipedia.org/wiki/INT_10H
+    mov AH, 13H 
+    int 10H    
                    
     MOV IS_GAME_TITLE_RENDERED, 1
-    
+                   
     pop AX
     pop BX
     pop CX
@@ -97,11 +113,12 @@ CHANGE_OPTION proc
     XOR AX, AX
                             
     ; https://www.stanislavs.org/helppc/int_16.html 
-    ; https://www.youtube.com/watch?v=8dYRlRjgqDY&ab_channel=ProgrammingDimension
+    ; https://www.youtube.com/watch?v=8dYRlRjgqDY&ab_channel=ProgrammingDimension       
+    
     ; Checar se alguma tecla foi clicada
     MOV AH, 01H
     INT 16H
-    JZ END_CHANGE_OPTION ; ZF = 0 => clicado | ZF = 1 => nada
+    JZ END_CHANGE_OPTION ; ZF = 0 => nao clicado | ZF = 1 => clicado
     
     ; Checar qual botao foi clicado (AL = ASCII Char)
     MOV AH, 00H                                      
@@ -114,13 +131,13 @@ CHANGE_OPTION proc
     JE CHANGE_OPTION_ACTION
     JMP END_CHANGE_OPTION
                    
-    ; Se qualquer tecla (pra cima ou pra baixo) foi clicada, apenas inverta o botão selecionado
+    ; Se qualquer tecla (pra cima ou pra baixo) foi clicada, apenas inverta o bot?o selecionado
     CHANGE_OPTION_ACTION:
     XOR AX,AX
     MOV AL, TITLE_SELECTED_BTN
     CMP AL, 1
-    JE CHANGE_OPTION_TO_2
-    JMP CHANGE_OPTION_TO_1
+    JE CHANGE_OPTION_TO_2      ; Se era 1, troca pra 2
+    JMP CHANGE_OPTION_TO_1     ; Se era 2, troca pra 1 
     
     CHANGE_OPTION_TO_1:
         MOV TITLE_SELECTED_BTN, 1
@@ -130,7 +147,7 @@ CHANGE_OPTION proc
         JMP REFRESH_OPTION_BTN 
     
     REFRESH_OPTION_BTN:    
-    call TITLE_BTNS  
+    call TITLE_BTNS             ; Renderiza novamente os botões em tela
  
     END_CHANGE_OPTION:   
     POP AX
@@ -151,7 +168,8 @@ proc SET_CARACTERE_A_SER_MOSTRADO
     MOV DL, ' '
     FIM_TESTE_BOTAO:
     RET
-endp SET_CARACTERE_A_SER_MOSTRADO
+endp SET_CARACTERE_A_SER_MOSTRADO       
+
 ; DH = Qual linha; BL = 1 | 2 - Botao atual
 proc SHOW_TITLE_PARENTHESIS
     PUSH DX            
@@ -179,7 +197,7 @@ proc SHOW_TITLE_PARENTHESIS
 endp SHOW_TITLE_PARENTHESIS  
 
 proc TITLE_BTNS     
-    ; Posiciona o cursor na posição para escrever o primeiro botão             
+    ; Posiciona o cursor na posi??o para escrever o primeiro bot?o             
     mov DL, 17
     mov DH, 19
                                 
@@ -191,7 +209,7 @@ proc TITLE_BTNS
     mov AH, 09H
     int 21H   
                                                                   
-    ; Posiciona o cursor na posição para escrever o segundo botão
+    ; Posiciona o cursor na posi??o para escrever o segundo bot?o
     mov DL, 17
     mov DH, 21
                            
@@ -205,14 +223,62 @@ proc TITLE_BTNS
                    
     ret
 endp
-
+       
+; DH = Y (linha). DL = X (coluna). CH = Cor
+proc DRAW_ONE_GHOST
+    PUSH AX
+    PUSH DS
+    PUSH BX     
+    PUSH CX
+            
+    MOV CL, DL  ; Salva o conteudo de DL em CL como um auxiliar pois a multiplicacao destroi o conteudo de DX
+            
+    XOR AX, AX
+    MOV AL, DH   ; Salva o conteudo de DH (linha a ser printada) para multiplicacao
+    MOV BX, 320  ; Precisa ser um registrador para o comando MUL
+    MUL BX       ; multiplica por 320 para encontrar a posição de memória na qual o pixel deve ser posto       
+    
+    XOR BX, BX
+    MOV BL, CL  ; Move a linha para BL (com BX zerado) para poder somar registradores de 16 bits (soma equivalente)
+    ADD AX, BX  ; Soma com a coluna para encontrar a posição correta   
+    
+    MOV BX, AX  ; Salva a posicao exata do pixel em BX
+     
+    MOV AX, VIDEO_BUFFER_SEGMENT ; Seta o segmento de buffer de vídeo (precisa do AX para essa atribuicao)
+    MOV DS, AX                          
+                                                                                  
+    MOV [BX], CH ; Poe o pixel na posicao correta com a cor correspondente    
+          
+    POP CX
+    POP BX
+    POP DS
+    POP AX
+    ret
+DRAW_ONE_GHOST endp
+        
+proc DRAW_INITIAL_SCREEN_GHOSTS   
+    PUSH DX
+           
+    MOV CH, 0CH ; VERMELHO_CLARO
+    MOV DL, 10H
+    MOV DH, 10H
+    
+    call DRAW_ONE_GHOST
+    
+    POP DX
+    ret
+DRAW_INITIAL_SCREEN_GHOSTS endp
+         
 proc INITIAL_SCREEN    
     MOV AL, IS_GAME_TITLE_RENDERED
     CMP AL, 0            
-    JNE START_NEXT_OPERATION
+    JNE START_NEXT_OPERATION ; Se já renderizou o titulo, não renderize novamente
+    
     call SHOW_TITLE                           
     call TITLE_BTNS      
-                     
+                                                                                 
+    call DRAW_INITIAL_SCREEN_GHOSTS
+                                                                                 
     START_NEXT_OPERATION:  
     call CHANGE_OPTION  
     
